6769855ff6002872dbc741f5c6b77fb2e1842262
pavit <hianandak@gmail.com>
2022-10-15 23:07:26 +0530

    session 90 done

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,12 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
   title = 'SkiNet';
-  products: any[];
-
-  constructor(private http: HttpClient){
-
-  }
-
+  constructor(){}
   ngOnInit(): void {
-
-    this.http.get('https://localhost:5001/api/products').subscribe((response:any) => {
-       this.products =response.data;
-      }, error => {console.log(error);
-      });
   }
 }
